30240a6088625cecdb726939554f2a5d17122779
Javohir564 <javohirmirzoyev1107@gmail.com>
2026-03-11 19:05:06 +0500

    fix aiogram

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-ADMIN_LOGIN = os.getenv("ADMIN_LOGIN", "admin")
-ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin123")
-ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "admin")
-DB_PATH = "database/database.db"
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_LOGIN = os.getenv("ADMIN_LOGIN")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+
+DB_PATH = "database/db.sqlite3"
